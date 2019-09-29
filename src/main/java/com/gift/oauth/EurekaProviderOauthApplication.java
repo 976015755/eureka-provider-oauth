9abd28811a505bccf551cae0ec07b23789ada39b
976015755 <976015755@qq.com>
2019-09-29 18:04:20 +0800

    first commit

---
+++ src/main/java/com/gift/oauth/EurekaProviderOauthApplication.java
@@ -0,0 +1,13 @@
+package com.gift.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaProviderOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaProviderOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gift/oauth/EurekaProviderOauthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gift.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaProviderOauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
